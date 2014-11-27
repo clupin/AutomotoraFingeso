1645a0086d7fabbe2259a476570d1336290d1d2e
clupin <cristian.espinozal@usach.cl>
2014-11-26 23:33:02 -0300

    agregado AutoNuevo

---
+++ Automotora-ejb/src/java/entityclass/AutoNuevo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entityclass;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Lupin
+ */
+@Entity
+public class AutoNuevo extends Automovil implements Serializable {
+    private static final long serialVersionUID = 1L;
+   
+    
+}
