1ce5d57050f315fc118980dd7054bc0be294a577
clupin <cristian.espinozal@usach.cl>
2014-11-27 10:35:45 -0300

    agregadas algunas entityclass mas

--- Automotora-ejb/src/java/entityclass/Gerente.java
+++ Automotora-ejb/src/java/entityclass/Gerente.java
@@ -7,16 +7,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Gerente extends Entidad implements Serializable {
     private static final long serialVersionUID = 1L;
     
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "usuario") 
+    private Usuario usuario;
+    
     @OneToMany(mappedBy = "AutoNuevo")
     @JoinColumn(name = "autosComprados") 
-    public List<AutoNuevo> autosComprados;
-
+    private List<AutoNuevo> autosComprados;
+    
     public List<AutoNuevo> getAutosComprados() {
         return autosComprados;
     }
@@ -24,6 +29,13 @@ public class Gerente extends Entidad implements Serializable {
     public void setAutosComprados(List<AutoNuevo> autosComprados) {
         this.autosComprados = autosComprados;
     }
-    
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
     
 }
